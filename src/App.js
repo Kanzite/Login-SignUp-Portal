c761e7e42e5ee0c349e9ee748219cc43eb34987b
Kanzite <rastogi_kush@yahoo.co.in>
2020-05-03 03:41:44 +0530

    Version One

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Overlay from './components/OverlayComponent';
+import Left from './components/LeftComponent';
+import Right from './components/RightComponent';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="main">
+			<div className="wrapper">
+				<div className="intro">
+				<Overlay />
+					<div className="containerwrapper">
+						<section className="left">
+							<Left />
+						</section>
+						<section className="right">
+							<Right />
+						</section>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
